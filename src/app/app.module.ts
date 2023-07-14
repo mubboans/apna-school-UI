e26570d365ec0082752954cfed7422b969711332
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-07-14 19:07:28 +0530

    sidebard done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
@@ -42,7 +41,6 @@ import { ChangePasswordComponent } from './core/auth-component/change-password/c
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // ButtonModule
     SharedModule,
     BrowserAnimationsModule
   ],
