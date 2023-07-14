9a972dda31b64e4ad5a0d1fe1dcbd258b694cd56
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-07-14 19:07:51 +0530

    part 2

--- src/app/core/common-component/header/header.component.ts
+++ src/app/core/common-component/header/header.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  showsidebar: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
 }
