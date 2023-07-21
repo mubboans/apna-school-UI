761c197d098ab6241f03e326f44849406ad3b43d
fy5mubashir <mubboans007@gmail.com>
2023-07-21 09:56:16 +0530

    interceptor error_response

--- src/app/core/interceptors/http-helper.interceptor.ts
+++ src/app/core/interceptors/http-helper.interceptor.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
+import { catchError, finalize } from 'rxjs/operators';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { LocalStorageDataService } from '../service/local-storage-data.service';
 
 @Injectable()
 export class HttpHelperInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(public localData:LocalStorageDataService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const token = this.localData.getToken();
+    if(token){
+      request=request.clone({     
+        setHeaders: { Authorization: `Bearer ${token}` }
+       });
+    }
+    return next.handle(request).pipe(finalize(()=>{
+
+    }),
+    catchError((error)=>{
+      
+      return throwError(error); 
+    }));
   }
 }
