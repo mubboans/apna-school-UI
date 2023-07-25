2213e9a12bd9f73a5afa841e576297e38b9077a4
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-07-25 16:27:34 +0530

    chart done

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { User } from 'src/app/core/models/user.model';
 import { LocalStorageDataService } from 'src/app/core/service/local-storage-data.service';
 
@@ -9,10 +10,60 @@ import { LocalStorageDataService } from 'src/app/core/service/local-storage-data
 })
 export class DashboardComponent implements OnInit {
   user:User;
-  constructor(public localData:LocalStorageDataService) { }
+  polardata: any;
+  linedata:any;
+  constructor(public localData:LocalStorageDataService,public messageService:MessageService) { }
 
   ngOnInit(): void {
+    this.polardata = {
+      datasets: [{
+          data: [
+              11,
+              16,
+              7,
+              3,
+              14
+          ],
+          backgroundColor: [
+              "#FF6384",
+              "#4BC0C0",
+              "#FFCE56",
+              "#E7E9ED",
+              "#36A2EB"
+          ],
+          label: 'My dataset'
+      }],
+      labels: [
+          "Red",
+          "Green",
+          "Yellow",
+          "Grey",
+          "Blue"
+      ]
+  }
+this.linedata={
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+      {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#4bc0c0'
+      },
+      {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#565656'
+      }
+  ]
+}
     this.user = this.localData.getUserLocalData();
   }
+  selectData(event) {
+    console.log(event.element)
+
+    this.messageService.add({severity: 'info', summary: 'Data Selected', detail: this.linedata.datasets[event.element._datasetIndex].data[event.element._index]});
+}
 
 }
